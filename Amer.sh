eff01f92d63da3c7a0e53c26e5de8478fdcbac63
Elharam4 <55320959+elostora@users.noreply.github.com>
2021-01-01 19:53:56 +0200

    Update Amer.sh

--- Amer.sh
+++ Amer.sh
@@ -3,8 +3,8 @@ p='\033[1;35m'
 clear
 echo -e "\033[1;32m"
 echo "         {Ctrl C} exit "
-echo "[1] Download Virus4 versions {4.6.8}"
-echo "[2] Download Virus4 versions {5.0.0}"
+echo "[1] Download Virus4. versions {4.6.8}"
+echo "[2] Download Virus4. versions {5.0.0}"
 echo ""
 echo -e "$p"
 read -p  "number-------> " download
